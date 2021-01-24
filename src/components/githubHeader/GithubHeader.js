9a42703eeec2790ac2f8f22da4dc177371774445
Dwayne Sutherland <dwayneasutherland@gmail.com>
2021-01-24 00:28:31 +0000

    Added media queries to make it responsive

--- src/components/githubHeader/GithubHeader.js
+++ src/components/githubHeader/GithubHeader.js
@@ -6,10 +6,11 @@ export default function GithubHeader(props){
 
     return(
         <div className="GithubHeader">
-            <a href="https://github.com/Delta-a-Sierra/">
+            <a href="https://github.com/Delta-a-Sierra/" target="_blank">
                 <img className="github-logo" src={githubIcon} alt="github logo" />
+                <h2>Delta-a-Sierra</h2>
             </a>
-            <h2>Delta-a-Sierra</h2>
+
         </div>
     )
 
